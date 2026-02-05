223a8dd30a244f89aa8862efda22c412c19ab7db
Kim-Ye-Sung <rhkwk6351@naver.com>
2026-02-05 17:29:42 +0900

    Day04 Commint
    4일차 수업내용 정리

---
+++ Day04/Chap09-Solution/Chap09-02-app/main.c
@@ -0,0 +1,17 @@
+﻿// 변수의 주소
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 100;    // int형 변수 선언 4byte
+	double b = 300.0;  // double형 변수 선언 8byte
+	char c = 12;	// char형 변수 선언 1byte
+
+	// & 메모리 주소를 나타내는 연산자
+	printf("int형 변수 a의 주소 : %u\n", &a);	// 주소연산자
+	printf("double형 변수 b의 주소 : %u\n", &b);
+	printf("char형 변수 c의 주소 : %u\n", &c); 
+
+	return 0;
+}
---
+++ Day04/Chap09-Solution/asd/asd.cpp
@@ -0,0 +1,58 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void ChangeTime(int* Time, string DestinationTime)
+{
+    for (int j = 0; j < DestinationTime.length(); j++)
+    {
+        if (j == 0)
+        {
+            *Time += (DestinationTime[j] - '0') * 1000;
+        }
+        else if (j == 1)
+        {
+            *Time += (DestinationTime[j] - '0') * 100;
+        }
+        else if (j == 3)
+        {
+            *Time += (DestinationTime[j] - '0') * 10;
+        }
+        else if (j == 4)
+        {
+            *Time += (DestinationTime[j] - '0');
+        }
+    }
+}
+
+string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
+    string answer = "";
+
+    int PosTime = 0;
+    int OpTime = 0;
+    int Op_EndTime = 0;
+
+    //for (int i = 0; i < commands.size(); i++)
+    //{
+        ChangeTime(&PosTime, pos);
+        ChangeTime(&OpTime, op_start);
+        ChangeTime(&Op_EndTime, op_end);
+    //}
+
+    cout << PosTime << endl;
+    cout << OpTime << endl;
+    cout << Op_EndTime << endl;
+
+    return answer;
+}
+
+int main()
+{   
+    //vector <string> a = { "next", "prev" };
+
+    //solution("34:33", "13:00", "00:55", "02:55", a);
+
+    cout << "737" - '0' << endl;
+    return 0;
+}
