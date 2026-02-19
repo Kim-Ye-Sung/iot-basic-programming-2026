f60876460bdfd919c1cbe23ee060ce09e08fe7c4
Kim-Ye-Sung <rhkwk6351@naver.com>
2026-02-19 17:26:52 +0900

    Day11 Commit
    11일차 수업 내용 정리

---
+++ Day10/ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> Coordinate_Cal(vector<int>a, vector<int>b)
+{
+    int c = a[0] - b[0];
+    int d = a[1] - b[1];
+
+    vector<int> e = { c,d };
+    return e;
+}
+
+
+int main()
+{
+    vector<int> aaa = { 1,2 };
+    vector<int> ccc = { 2,4 };
+
+    vector<int> a = Coordinate_Cal(aaa, ccc);
+
+    cout << a[0] << a[1] << endl;
+}
+
